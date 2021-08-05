241e6f0da5734e08be3251e3e246734c05b75610
gtang1626 <gtang1626@gmail.com>
2021-08-05 16:53:02 +0000

    Show multiple gifs

--- public/js/giphy.js
+++ public/js/giphy.js
@@ -17,10 +17,8 @@ const convertMsgToGif = (message, messageId) => {
     let gifUrl = "";
     const display = document.getElementById(`${messageId}`);
     
-    let imgWidth = 25;
-    if (keyWords.length > 10){
-        imgWidth = 100/keyWords.length;
-    }
+    let imgWidth = 20;
+    let counter = 0;
     
     for(const word of keyWords){
         urlToFetch = `https://api.giphy.com/v1/gifs/search?api_key=${myKey}&q=${word}&limit=25&offset=0&rating=g&lang=en`;
@@ -28,12 +26,12 @@ const convertMsgToGif = (message, messageId) => {
             .then(response => response.json())
             .then(myJson => {
                 let random = Math.floor(Math.random()*25)
-                console.log(random)
                 let imgUrl = myJson.data[random].images.original.url;
-                
-                let html = ""
-                    html += `<img src="${imgUrl}" alt="${word}" style="width:${imgWidth}%;"/>`
-                display.innerHTML = html
+                counter ++;
+                display.innerHTML += `<img src="${imgUrl}" alt="${word}" style="width:${imgWidth}%;"/>`
+                if (counter % 5 === 0){
+                    display.innerHTML += "<br>";
+                }
             })
         .catch(error => {
             console.log(error);
